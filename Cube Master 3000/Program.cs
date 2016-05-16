df1c6dbeb4a29f124a28f4a73625ea6f33e01808
Isaak Azim Munnee <isaakmunnee@gmail.com>
2016-05-16 22:27:55 +0800

    Add project files.

---
+++ Cube Master 3000/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube_Master_3000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
